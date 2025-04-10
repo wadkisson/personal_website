1f3c1f5399368c6f6c0a15b82ffeabba7cd874ab
wadkisson <adkisson@wustl.edu>
2025-04-10 12:46:10 -0500

    Fix website for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  base: '/personal_website/', // important: must match your GitHub repo name
   server: {
     open: true
   }
